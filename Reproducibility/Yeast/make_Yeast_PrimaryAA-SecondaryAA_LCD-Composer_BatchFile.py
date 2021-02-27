1865ad3da3af0f5184a884ae87c29259ea474da6
RossLabCSU <49657843+RossLabCSU@users.noreply.github.com>
2021-02-27 08:01:03 -0700

    Add files via upload

---
+++ Reproducibility/Yeast/make_Yeast_PrimaryAA-SecondaryAA_LCD-Composer_BatchFile.py
@@ -0,0 +1,18 @@
+
+amino_acids = 'ACDEFGHIKLMNPQRSTVWY'
+
+def main():
+
+    output = open('Run_LCD-Composer_Yeast_PrimaryAA-SeoncdaryAA.bat', 'w')
+
+    file = 'UP000002311_Scerevisiae_NoIsoforms'
+    for aa in amino_acids:
+        for secondary_aa in amino_acids.replace(aa, ''):
+            results_file = 'Scerevisiae_' + aa + '-primary_' + secondary_aa + '-secondary_LCD-Composer_RESULTS'
+            output.write('python LCD-Composer.py ' + file + '.fasta ' + results_file + ' -a ' + aa + '_' + secondary_aa + ' -c 40_20\n')
+                
+    output.close()
+    
+
+if __name__ == '__main__':
+    main()
