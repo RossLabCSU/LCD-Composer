30273211a037013d3cc6c5b6536087aaefd4c925
RossLabCSU <49657843+RossLabCSU@users.noreply.github.com>
2021-02-28 08:57:57 -0700

    Add files via upload

---
+++ Reproducibility/Yeast/gather_LCDproteins_PrimaryAA-SecondaryAA.py
@@ -0,0 +1,29 @@
+
+
+def main():
+
+    amino_acids = 'ACDEFGHIKLMNPQRSTVWY'
+    for aa in amino_acids:
+        for sec_aa in amino_acids.replace(aa, ''):
+            h = open('Scerevisiae_' + aa + '-primary_' + sec_aa + '-secondary_LCD-Composer_RESULTS.tsv')
+            output = open('Scerevisiae_' + aa + '-primary_' + sec_aa + '-secondary_LCD-containing_proteins.txt', 'w')
+            
+            for i in range(7):
+                h.readline()
+            header = h.readline()
+                
+            prots = set()
+            for line in h:
+                id, seq, boundaries, final_comp, final_lindisp, *remainder = line.rstrip().split('\t')
+                junk, uniprot_id, junk = id.split('|')
+                prots.add( uniprot_id )
+            h.close()
+            
+            for prot in prots:
+                output.write(prot + '\n')
+                
+            output.close()
+
+
+if __name__ == '__main__':
+    main()
