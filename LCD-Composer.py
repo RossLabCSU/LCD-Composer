e66ba756ff312a120067df3888ba63f191b34518
RossLabCSU <49657843+RossLabCSU@users.noreply.github.com>
2021-06-03 15:31:57 -0600

    Update LCD-Composer.py

--- LCD-Composer.py
+++ LCD-Composer.py
@@ -2,7 +2,7 @@
 """
 Description:
     LCD-Composer is a composition-based method for identifying low-complexity domains in protein sequences.
-    Refer to Cascarina et. al. (2020) (INCLUDE LINK WHEN PUBLISHED) for a complete 
+    Refer to Cascarina et. al. (2021) (https://academic.oup.com/nargab/article/3/2/lqab048/6285187) for a complete 
     description of algorithm testing and application.
     Documentation for LCD-Composer is available at https://github.com/RossLabCSU/LCD-Composer.
 ============================================================================================
